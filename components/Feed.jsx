e9fc2cf621315a0ccb2d7b5000dcfdf988a41072
salman6abbas <salman6abbas@gmail.com>
2023-06-01 17:55:31 +0500

    my nextjs app

---
+++ components/Feed.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+    const newPosts = posts.filter((post) => {
+      return (
+        post.prompt.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        post.creator.username.includes(e.target.value.toLowerCase()) ||
+        post.tag.includes(e.target.value.toLowerCase())
+      );
+    });
+    setFilteredPosts(newPosts);
+  };
+
+  const handleTagClick = (tag) => {
+    setSearchText(tag);
+    let newPosts = posts.filter((post) => post.tag.includes(tag));
+    setFilteredPosts(newPosts);
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+      setFilteredPosts(data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={filteredPosts} handleTagClick={handleTagClick} />
+    </section>
+  );
+};
+
+export default Feed;
